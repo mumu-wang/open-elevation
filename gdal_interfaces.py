061e0e247993fa4c1aa489248221cef9d97ace80
linwang <linwang@telenavsoftware.com>
2022-03-29 13:18:38 +0800

    enhance service to support 30, 120 and 240 meters precision.

--- gdal_interfaces.py
+++ gdal_interfaces.py
@@ -5,14 +5,36 @@ from os import listdir
 from os.path import isfile, join, getsize
 import json
 from rtree import index
+import time
+
+DEFAULT_FMT = '[cost time is {elapsed:0.6f}s] {name}'
+
+
+def clock(fmt=DEFAULT_FMT):
+    def decorate(func):
+        def clocked(*_args):
+            t0 = time.perf_counter()
+            _result = func(*_args)
+            name = func.__name__
+            elapsed = time.perf_counter() - t0
+            print(fmt.format(**locals()))
+            return _result
+
+        return clocked
+
+    return decorate
+
 
 # Originally based on https://stackoverflow.com/questions/13439357/extract-point-from-raster-in-gdal
 class GDALInterface(object):
     SEA_LEVEL = 0
-    def __init__(self, tif_path):
+
+    def __init__(self, tif_path, cache_all=False):
         super(GDALInterface, self).__init__()
         self.tif_path = tif_path
         self.loadMetadata()
+        if cache_all:
+            self.load_all()
 
     def get_corner_coords(self):
         ulx, xres, xskew, uly, yskew, yres = self.geo_transform
@@ -44,8 +66,6 @@ class GDALInterface(object):
         self.geo_transform_inv = (gt[0], gt[5] / dev, -gt[2] / dev,
                                   gt[3], -gt[4] / dev, gt[1] / dev)
 
-
-
     @lazy
     def points_array(self):
         b = self.src.GetRasterBand(1)
@@ -53,7 +73,6 @@ class GDALInterface(object):
 
     def print_statistics(self):
         print(self.src.GetRasterBand(1).GetStatistics(True, True))
-
 
     def lookup(self, lat, lon):
         try:
@@ -76,6 +95,33 @@ class GDALInterface(object):
             print(e)
             return self.SEA_LEVEL
 
+    def lookup_cache(self, lat, lon):
+        try:
+
+            # get coordinate of the raster
+            xgeo, ygeo, zgeo = (lon, lat, 0)
+            # convert it to pixel/line on band
+            u = xgeo - self.geo_transform_inv[0]
+            v = ygeo - self.geo_transform_inv[3]
+            # FIXME this int() is probably bad idea, there should be half cell size thing needed
+            xpix = int(self.geo_transform_inv[1] * u + self.geo_transform_inv[2] * v)
+            ylin = int(self.geo_transform_inv[4] * u + self.geo_transform_inv[5] * v)
+
+            # look the value up
+            v = self.cache_array[ylin, xpix]
+            return v if v != -32768 else self.SEA_LEVEL
+        except Exception as e:
+            print(e)
+            return self.SEA_LEVEL
+
+    def load_all(self):
+        b = self.src.GetRasterBand(1)
+        self.cache_array = b.ReadAsArray().astype('int16')
+        self.close()
+        self.coordinate_transform = None
+        self.points_array = None
+        self.tif_path = None
+
     def close(self):
         self.src = None
 
@@ -85,8 +131,11 @@ class GDALInterface(object):
     def __exit__(self, type, value, traceback):
         self.close()
 
+
 class GDALTileInterface(object):
-    def __init__(self, tiles_folder, summary_file, open_interfaces_size=5):
+    SEA_LEVEL = 0
+
+    def __init__(self, tiles_folder, summary_file, open_interfaces_size=5, cache_all=False):
         super(GDALTileInterface, self).__init__()
         self.tiles_folder = tiles_folder
         self.summary_file = summary_file
@@ -94,6 +143,7 @@ class GDALTileInterface(object):
         self.cached_open_interfaces = []
         self.cached_open_interfaces_dict = {}
         self.open_interfaces_size = open_interfaces_size
+        self.cache_all = cache_all
 
     def _open_gdal_interface(self, path):
         if path in self.cached_open_interfaces_dict:
@@ -127,8 +177,8 @@ class GDALTileInterface(object):
     def create_summary_json(self):
         all_coords = []
         for file in self._all_files():
-            full_path = join(self.tiles_folder,file)
-            print('Processing %s ... (%s MB)' % (full_path, getsize(full_path) / 2**20))
+            full_path = join(self.tiles_folder, file)
+            print('Processing %s ... (%s MB)' % (full_path, getsize(full_path) / 2 ** 20))
             i = self._open_gdal_interface(full_path)
             coords = i.get_corner_coords()
 
@@ -137,21 +187,18 @@ class GDALTileInterface(object):
             all_coords += [
                 {
                     'file': full_path,
-                    'coords': ( lmin,  # latitude min
-                                lmax,  # latitude max
-                                lngmin,  # longitude min
-                                lngmax,  # longitude max
-
-                                )
+                    'coords': (lmin,  # latitude min
+                               lmax,  # latitude max
+                               lngmin,  # longitude min
+                               lngmax,  # longitude max
+                               )
                 }
             ]
             print('\tDone! LAT (%s,%s) | LNG (%s,%s)' % (lmin, lmax, lngmin, lngmax))
 
         with open(self.summary_file, 'w') as f:
             json.dump(all_coords, f)
-
         self.all_coords = all_coords
-
         self._build_index()
 
     def read_summary_json(self):
@@ -161,20 +208,28 @@ class GDALTileInterface(object):
         self._build_index()
 
     def lookup(self, lat, lng):
-        nearest = list(self.index.nearest((lat, lng), 1, objects=True))
-
+        nearest = list(self.index.intersection((lat, lng), objects=True))
         if not nearest:
-            raise Exception('Invalid latitude/longitude')
+            return self.SEA_LEVEL
         else:
             coords = nearest[0].object
+            if not self.cache_all:
+                gdal_interface = self._open_gdal_interface(coords['file'])
+                return int(gdal_interface.lookup(lat, lng))
+            else:
+                return int(coords['interface'].lookup_cache(lat, lng))
 
-            gdal_interface = self._open_gdal_interface(coords['file'])
-            return int(gdal_interface.lookup(lat, lng))
-
+    @clock()
     def _build_index(self):
         print('Building spatial index ...')
         index_id = 1
         for e in self.all_coords:
             e['index_id'] = index_id
             left, bottom, right, top = (e['coords'][0], e['coords'][2], e['coords'][1], e['coords'][3])
-            self.index.insert( index_id, (left, bottom, right, top), obj=e)
+            if self.cache_all:
+                interface = GDALInterface(e['file'], cache_all=True)
+                e['interface'] = interface
+                del e['file']
+                del e['index_id']
+                del e['coords']
+            self.index.insert(index_id, (left, bottom, right, top), obj=e)
--- server.py
+++ server.py
@@ -12,6 +12,7 @@ class InternalException(ValueError):
     """
     pass
 
+
 print('Reading config file ...')
 parser = configparser.ConfigParser()
 parser.read('config.ini')
@@ -19,28 +20,46 @@ parser.read('config.ini')
 HOST = parser.get('server', 'host')
 PORT = parser.getint('server', 'port')
 NUM_WORKERS = parser.getint('server', 'workers')
-DATA_FOLDER = parser.get('server', 'data-folder')
+DATA_FOLDER_30 = parser.get('server', 'data-folder_30')
+DATA_FOLDER_120 = parser.get('server', 'data-folder_120')
+DATA_FOLDER_240 = parser.get('server', 'data-folder_240')
 OPEN_INTERFACES_SIZE = parser.getint('server', 'open-interfaces-size')
+URL_ENDPOINT_30 = parser.get('server', 'endpoint_30')
+URL_ENDPOINT_120 = parser.get('server', 'endpoint_120')
+URL_ENDPOINT_240 = parser.get('server', 'endpoint_240')
 URL_ENDPOINT = parser.get('server', 'endpoint')
 ALWAYS_REBUILD_SUMMARY = parser.getboolean('server', 'always-rebuild-summary')
 CERTS_FOLDER = parser.get('server', 'certs-folder')
+# always set false to keep system safe currently
+# CACHE_ALL_TIFF_FILES = parser.getboolean('server', 'cache_all_tiff_files')
+CACHE_ALL_TIFF_FILES = False
 CERT_FILE = '%s/cert.crt' % CERTS_FOLDER
 KEY_FILE = '%s/cert.key' % CERTS_FOLDER
+
+
+def build_summary(gdal_interface):
+    if gdal_interface.has_summary_json() and not ALWAYS_REBUILD_SUMMARY:
+        print('Re-using existing summary JSON')
+        gdal_interface.read_summary_json()
+    else:
+        print('Creating summary JSON ...')
+        gdal_interface.create_summary_json()
 
 
 """
 Initialize a global interface. This can grow quite large, because it has a cache.
 """
-interface = GDALTileInterface(DATA_FOLDER, '%s/summary.json' % DATA_FOLDER, OPEN_INTERFACES_SIZE)
+interface_30 = GDALTileInterface(DATA_FOLDER_30, '%s/summary.json' % DATA_FOLDER_30, OPEN_INTERFACES_SIZE, CACHE_ALL_TIFF_FILES)
+build_summary(interface_30)
 
-if interface.has_summary_json() and not ALWAYS_REBUILD_SUMMARY:
-    print('Re-using existing summary JSON')
-    interface.read_summary_json()
-else:
-    print('Creating summary JSON ...')
-    interface.create_summary_json()
+interface_120 = GDALTileInterface(DATA_FOLDER_120, '%s/summary.json' % DATA_FOLDER_120, OPEN_INTERFACES_SIZE, CACHE_ALL_TIFF_FILES)
+build_summary(interface_120)
 
-def get_elevation(lat, lng):
+
+interface_240 = GDALTileInterface(DATA_FOLDER_240, '%s/summary.json' % DATA_FOLDER_240, OPEN_INTERFACES_SIZE, CACHE_ALL_TIFF_FILES)
+build_summary(interface_240)
+
+def get_elevation(lat, lng, gdal_interface):
     """
     Get the elevation at point (lat,lng) using the currently opened interface
     :param lat:
@@ -48,7 +67,7 @@ def get_elevation(lat, lng):
     :return:
     """
     try:
-        elevation = interface.lookup(lat, lng)
+        elevation = gdal_interface.lookup(lat, lng)
     except:
         return {
             'latitude': lat,
@@ -96,7 +115,7 @@ def query_to_locations():
     if not locations:
         raise InternalException(json.dumps({'error': '"Locations" is required.'}))
 
-    return [lat_lng_from_location(l) for l in locations.split('|')]
+    return [lat_lng_from_location(l) for l in locations.split(';')]
 
 
 def body_to_locations():
@@ -115,14 +134,14 @@ def body_to_locations():
     latlng = []
     for l in locations:
         try:
-            latlng += [ (l['latitude'],l['longitude']) ]
+            latlng += [(l['latitude'], l['longitude'])]
         except KeyError:
             raise InternalException(json.dumps({'error': '"%s" is not in a valid format.' % l}))
 
     return latlng
 
 
-def do_lookup(get_locations_func):
+def do_lookup(get_locations_func, gdal_interface):
     """
     Generic method which gets the locations in [(lat,lng),(lat,lng),...] format by calling get_locations_func
     and returns an answer ready to go to the client.
@@ -130,33 +149,87 @@ def do_lookup(get_locations_func):
     """
     try:
         locations = get_locations_func()
-        return {'results': [get_elevation(lat, lng) for (lat, lng) in locations]}
+        return {'results': [get_elevation(lat, lng, gdal_interface) for (lat, lng) in locations]}
     except InternalException as e:
         response.status = 400
         response.content_type = 'application/json'
         return e.args[0]
 
+
 # For CORS
+@route(URL_ENDPOINT_30, method=['OPTIONS'])
+def cors_handler():
+    return {}
+
+
+@route(URL_ENDPOINT_30, method=['GET'])
+def get_lookup():
+    """
+    GET method. Uses query_to_locations.
+    :return:
+    """
+    return do_lookup(query_to_locations, interface_30)
+
+
+@route(URL_ENDPOINT_30, method=['POST'])
+def post_lookup():
+    """
+    GET method. Uses body_to_locations.
+    :return:
+    """
+    return do_lookup(body_to_locations, interface_30)
+
+
+# For CORS
+@route(URL_ENDPOINT_120, method=['OPTIONS'])
+def cors_handler():
+    return {}
+
+
+@route(URL_ENDPOINT_120, method=['GET'])
+def get_lookup():
+    """
+    GET method. Uses query_to_locations.
+    :return:
+    """
+    return do_lookup(query_to_locations, interface_120)
+
+
+@route(URL_ENDPOINT_120, method=['POST'])
+def post_lookup():
+    """
+    GET method. Uses body_to_locations.
+    :return:
+    """
+    return do_lookup(body_to_locations, interface_120)
+
+
+# For CORS
+@route(URL_ENDPOINT_240, method=['OPTIONS'])
 @route(URL_ENDPOINT, method=['OPTIONS'])
 def cors_handler():
     return {}
 
+
+@route(URL_ENDPOINT_240, method=['GET'])
 @route(URL_ENDPOINT, method=['GET'])
 def get_lookup():
     """
     GET method. Uses query_to_locations.
     :return:
     """
-    return do_lookup(query_to_locations)
+    return do_lookup(query_to_locations, interface_240)
 
 
+@route(URL_ENDPOINT_240, method=['POST'])
 @route(URL_ENDPOINT, method=['POST'])
 def post_lookup():
     """
     GET method. Uses body_to_locations.
     :return:
     """
-    return do_lookup(body_to_locations)
+    return do_lookup(body_to_locations, interface_240)
+
 
 if os.path.isfile(CERT_FILE) and os.path.isfile(KEY_FILE):
     print('Using HTTPS')
